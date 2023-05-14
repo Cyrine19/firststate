81e71ebf66e4541ff1a6b541fb99d7c23e90886e
“cyrine19” <syrineba9@gmail.com>
2023-05-14 10:17:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import ProfilePhoto from './components/img1.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Cyrine Benali',
+      bio: 'Lorem ipsum dolor sit amet.',
+      imgSrc: ProfilePhoto ,
+      profession: 'Developer'
+    },
+    show: false,
+    mountedAt: null,
+  };
+
+  componentDidMount() {
+    this.startInterval();
+  };
+  componentWillUnmount() {
+    this.stopInterval();
+  }
+  startInterval(){
+    const {mountedAt} =this.state;
+    const currentTime= new Date() ;
+    const time=Math.floor(((currentTime - mountedAt )/1000)) ;
+  console.log(time);
+    return time;
+  }
+ 
+  stopInterval=()=>{
+    clearInterval(this.interval);
+  };
+ toggleShow = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+    
+  };
+
+  calculateElapsedTime = () => {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  };
+
+  render() {
+
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show } = this.state;
+
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        <p>Component mounted {this.startInterval()} mseconds ago.</p>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt={fullName} />
+            <p>{profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  } 
+ };
 
 export default App;
